17c67c623c6b5495e591cca5e0d862300d07cbbd
Victor Calderon <victor.calderon90@gmail.com>
2017-09-27 11:32:27 -0500

    Added arguments for script

--- src/data/One_halo_conformity/one_halo_mark_correlation.py
+++ src/data/One_halo_conformity/one_halo_mark_correlation.py
@@ -25,11 +25,51 @@ matplotlib.use( 'Agg' )
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 plt.rc('text', usetex=True)
-import seaborn.apionly as sns
+import seaborn as sns
+# sns.set()
 from progressbar import (Bar, ETA, FileTransferSpeed, Percentage, ProgressBar,
                         ReverseBar, RotatingMarker)
 
 # Extra-modules
+import argparse
+from argparse import ArgumentParser
+def _str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def _check_pos_val(val, val_min=0):
+    """
+    Checks if value is larger than `val_min`
+
+    Parameters
+    ----------
+    val: int or float
+        value to be evaluated by `val_min`
+
+    val_min: float or int, optional (default = 0)
+        minimum value that `val` can be
+
+    Returns
+    -------
+    ival: float
+        value if `val` is larger than `val_min`
+
+    Raises
+    -------
+    ArgumentTypeError: Raised if `val` is NOT larger than `val_min`
+    """
+    ival = float(val)
+    if ival <= val_min:
+        msg  = '`{0}` is an invalid input!'.format(ival)
+        msg += '`val` must be larger than `{0}`!!'.format(val_min)
+        raise argparse.ArgumentTypeError(msg)
+
+    return ival
+
 def get_parser():
     """
     Get parser object for `eco_mocks_create.py` script.
@@ -44,11 +84,104 @@ def get_parser():
     parser = ArgumentParser(description=description_msg)
     # 
     parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-    parser.add_argument('-namevar', '--long-name',
-                        dest='variable_name',
-                        help='Description of variable',
-                        type=float,
+    ## SDSS Sample
+    parser.add_argument('-sample',
+                        dest='sample',
+                        help='SDSS Luminosity sample to analyze',
+                        type=int,
+                        choices=[19,20,21],
+                        default=19)
+    ## SDSS Kind
+    parser.add_argument('-kind',
+                        dest='sdss_kind',
+                        help='Type of data being analyzed.',
+                        type=str,
+                        choices=['data','mocks'],
+                        default='data')
+    ## Correlation Pair Type
+    parser.add_argument('-pairtype',
+                        dest='corr_pair_type',
+                        help='Types of galaxy pairs to keep for the MFC and ',
+                        type=str,
+                        choices=['cen_sat', 'all'],
+                        default='cen_sat')
+    ## Shuffling Marks
+    parser.add_argument('-shuffle',
+                        dest='shuffle_marks',
+                        help='Option for shuffling marks of Cens. and Sats.',
+                        choices=['cen_sh', 'sat_sh', 'censat_sh'],
+                        default='censat_sh')
+    ## Rpmin and Rpmax
+    parser.add_argument('-rpmin',
+                        dest='rpmin',
+                        help='Minimum value for projected distance `rp`',
+                        type=_check_pos_val,
+                        default=0.01)
+    parser.add_argument('-rpmax',
+                        dest='rpmax',
+                        help='Maximum value for projected distance `rp`',
+                        type=_check_pos_val,
+                        default=10)
+    ## Number of `rp` bins
+    parser.add_argument('-nrp',
+                        dest='nrpbins',
+                        help='Number of bins for projected distance `rp`',
+                        type=int,
+                        default=10)
+    ## Minimum Number of Galaxies in 1 group
+    parser.add_argument('-nmin',
+                        dest='ngals_min',
+                        help='Minimum number of galaxies in a galaxy group',
+                        type=int,
+                        default=2)
+    ## Bin in Group mass
+    parser.add_argument('-mg',
+                        dest='Mg_bin',
+                        help='Bin width for the group masses',
+                        type=_check_pos_val,
+                        default=0.4)
+    ## Pimax
+    parser.add_argument('-pimax',
+                        dest='pimax',
+                        help='Value for `pimax` for the proj. corr. function',
+                        type=_check_pos_val,
+                        default=20)
+    ## Logarithm of the galaxy property
+    parser.add_argument('-log',
+                        dest='prop_log',
+                        help='Use `log` or `non-log` units for `M*` and `sSFR`',
+                        type=str,
+                        choices=['log', 'nonlog'],
+                        default='log')
+    ## Mock Start
+    parser.add_argument('-start',
+                        dest='mocks_start',
+                        help='Starting index of mock catalogues to use',
+                        type=int,
+                        choices=range(100),
+                        metavar='[0-99]',
                         default=0)
+    ## Mock Finish
+    parser.add_argument('-finish',
+                        dest='mocks_finish',
+                        help='Finishing index of mock catalogues to use',
+                        type=int,
+                        choices=range(100),
+                        metavar='[0-99]',
+                        default=1)
+    ## `Perfect Catalogue` Option
+    parser.add_argument('-perf',
+                        dest='perf_opt',
+                        help='Option for using a `Perfect` catalogue',
+                        type=str,
+                        choices=['yes', 'no'],
+                        default='no')
+    ## Option for removing file
+    parser.add_argument('-remove',
+                        dest='remove_files',
+                        help='Delete pickle file containing pair counts',
+                        type=_str2bool,
+                        default=False)
     ## Parsing Objects
     args = parser.parse_args()
 
@@ -63,9 +196,8 @@ def main():
 
 # Main function
 if __name__=='__main__':
-    from argparse import ArgumentParser
-
     ## Input arguments
     args = get_parser()
+    print(args)
     # Main Function
     main()
