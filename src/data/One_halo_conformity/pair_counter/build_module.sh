471d059604aaccedc089cadb4b02d60c4e2329e0
Victor Calderon <victor.calderon90@gmail.com>
2017-10-04 08:29:26 -0500

    Copied the executable one folder up

--- src/data/One_halo_conformity/pair_counter/build_module.sh
+++ src/data/One_halo_conformity/pair_counter/build_module.sh
@@ -4,5 +4,6 @@ python setup.py build_ext --inplace
 rm ./*.so
 cp ./pair_counter/pair_counter_rp*.so ./
 mv ./pair_counter_rp*.so ./pair_counter_rp.so
+cp ./pair_counter_rp.so ../
 rm -r ./pair_counter
 rm -r ./build
