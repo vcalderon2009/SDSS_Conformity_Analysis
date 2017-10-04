7c6fc0bd4c3556750b5adc83d7457a4b4f94ea76
Victor Calderon <victor.calderon90@gmail.com>
2017-10-04 08:34:43 -0500

    Removing files

--- src/data/One_halo_conformity/pair_counter/build_module.sh
+++ src/data/One_halo_conformity/pair_counter/build_module.sh
@@ -3,8 +3,8 @@
 python setup.py build_ext --inplace
 rm ./*.so
 rm ../*.so
-# cp ./pair_counter/pair_counter_rp*.so ./
-# mv ./pair_counter_rp*.so ./pair_counter_rp.so
-# cp ./pair_counter_rp.so ../
-# rm -r ./pair_counter
-# rm -r ./build
+cp ./pair_counter/pair_counter_rp*.so ./
+mv ./pair_counter_rp*.so ./pair_counter_rp.so
+cp ./pair_counter_rp.so ../
+rm -r ./pair_counter
+rm -r ./build
