f692ba1a8c868009d73ef3ab2c9e33ab7baf49f7
Victor Calderon <victor.calderon90@gmail.com>
2017-10-16 17:16:22 -0500

    Modified commands

--- src/data/Two_halo_conformity/pair_counter_rp/build_module.sh
+++ src/data/Two_halo_conformity/pair_counter_rp/build_module.sh
@@ -5,27 +5,18 @@ rm ./*.so
 (rm -r ./build && rm -r ./${name_mod}) || (printf "")
 ## Running Cython compilation
 python setup.py build_ext --inplace
-## Copying python module
-(   cp ./${name_mod}/*.so ./ &&
+##
+## Compiling Cython module
+##
+(   ## Copying python module to current folder
+    cp ./${name_mod}/*.so ./ &&
+    ## Changing name
     mv ./${name_mod}*.so  ./${name_mod}.so &&
+    ## Deleting extra folders
     rm -rf ./${name_mod} &&
     rm -rf ./build) || (
     
+    ## Renaming python module
     mv ./${name_mod}*.so ./${name_mod}.so &&
-    rm -rf ./build &&
-    printf "\n\n\n Ahora si se pudo" )
-
-
-
-
-## Running Cython compilation
-# python setup.py build_ext --inplace
-## Copying python module
-# printf "\n\n"
-# (echo "Test" && printf "\n\n Otro test\n" && rm ./${name_mod}_Testing) || (
-#     printf "Esta cosa no resulto\n\n\n")
-# (echo"cp ./${name_mod}/*so ./ && mv ./${name_mod}/*.so ./${name_mod}.so") || (
-    # echo"mv ./${name_mod}*.so ./${name_mod}.so" )
-## Deleting extra folders
-# rm -r ./${name_mod}
-# rm -r ./build
+    ## Deleting extra folders produced by Cython
+    rm -rf ./build )
