941108bfc563c0c47f676c8cd7095044ded315b1
Victor Calderon <victor.calderon90@gmail.com>
2017-10-16 17:17:06 -0500

    Modified the structure of the script by allowing it to be compiled on mac or linux

--- src/data/One_halo_conformity/pair_counter_rp/build_module.sh
+++ src/data/One_halo_conformity/pair_counter_rp/build_module.sh
@@ -2,11 +2,21 @@
 name_mod='pair_counter_rp'
 ## Removing previous *.so files
 rm ./*.so
+(rm -r ./build && rm -r ./${name_mod}) || (printf "")
 ## Running Cython compilation
 python setup.py build_ext --inplace
-## Copying python module
-cp ./${name_mod}/${pair_counter_rp}*.so ./
-mv ./${name_mod}*.so ./${name_mod}.so
-## Deleting extra folders
-rm -r ./${name_mod}
-rm -r ./build
+##
+## Compiling Cython module
+##
+(   ## Copying python module to current folder
+    cp ./${name_mod}/*.so ./ &&
+    ## Changing name
+    mv ./${name_mod}*.so  ./${name_mod}.so &&
+    ## Deleting extra folders
+    rm -rf ./${name_mod} &&
+    rm -rf ./build) || (
+    
+    ## Renaming python module
+    mv ./${name_mod}*.so ./${name_mod}.so &&
+    ## Deleting extra folders produced by Cython
+    rm -rf ./build )
