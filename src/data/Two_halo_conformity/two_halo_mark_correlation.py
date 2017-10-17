58d40ef285d598ac7f0b231e89bfe4342e428f6a
Victor Calderon <victor.calderon90@gmail.com>
2017-10-17 07:20:26 -0500

    Changed limits for `catl_start` and `catl_finish` up to 100

--- src/data/Two_halo_conformity/two_halo_mark_correlation.py
+++ src/data/Two_halo_conformity/two_halo_mark_correlation.py
@@ -301,7 +301,7 @@ def get_parser():
                         dest='catl_start',
                         help='Starting index of mock catalogues to use',
                         type=int,
-                        choices=range(100),
+                        choices=range(101),
                         metavar='[0-99]',
                         default=0)
     ## Mock Finish
@@ -309,7 +309,7 @@ def get_parser():
                         dest='catl_finish',
                         help='Finishing index of mock catalogues to use',
                         type=int,
-                        choices=range(100),
+                        choices=range(101),
                         metavar='[0-99]',
                         default=1)
     ## `Perfect Catalogue` Option
