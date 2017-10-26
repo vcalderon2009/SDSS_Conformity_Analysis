94ab033bac47b3e71dcb4a92b37b0b44a5a28c8f
Victor Calderon <victor.calderon90@gmail.com>
2017-10-26 16:06:32 -0500

    Initial commit of the 1-halo Quenched Fraction script

---
+++ src/data/One_halo_conformity/one_halo_conformity_quenched_fractions.py
@@ -0,0 +1,455 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Victor Calderon
+# Created      : 10/24/2017
+# Last Modified: 10/24/2017
+# Vanderbilt University
+from __future__ import print_function, division, absolute_import
+__author__     =['Victor Calderon']
+__copyright__  =["Copyright 2017 Victor Calderon, "]
+__email__      =['victor.calderon@vanderbilt.edu']
+__maintainer__ =['Victor Calderon']
+"""
+Computes the 1-halo `Quenched` fractions for SDSS DR7
+"""
+# Importing Modules
+import custom_utilities_python as cu
+import numpy as num
+import math
+import os
+import sys
+import pandas as pd
+import pickle
+#sns.set()
+from progressbar import (Bar, ETA, FileTransferSpeed, Percentage, ProgressBar,
+                        ReverseBar, RotatingMarker)
+
+# Extra-modules
+from argparse import ArgumentParser
+from argparse import HelpFormatter
+from operator import attrgetter
+
+## Functions
+
+## --------- PARSING ARGUMENTS ------------##
+
+class SortingHelpFormatter(HelpFormatter):
+    def add_arguments(self, actions):
+        """
+        Modifier for `argparse` help parameters, that sorts them alphabetically
+        """
+        actions = sorted(actions, key=attrgetter('option_strings'))
+        super(SortingHelpFormatter, self).add_arguments(actions)
+
+def _str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def _check_pos_val(val, val_min=0):
+    """
+    Checks if value is larger than `val_min`
+
+    Parameters
+    ----------
+    val: int or float
+        value to be evaluated by `val_min`
+
+    val_min: float or int, optional (default = 0)
+        minimum value that `val` can be
+
+    Returns
+    -------
+    ival: float
+        value if `val` is larger than `val_min`
+
+    Raises
+    -------
+    ArgumentTypeError: Raised if `val` is NOT larger than `val_min`
+    """
+    ival = float(val)
+    if ival <= val_min:
+        msg  = '`{0}` is an invalid input!'.format(ival)
+        msg += '`val` must be larger than `{0}`!!'.format(val_min)
+        raise argparse.ArgumentTypeError(msg)
+
+    return ival
+
+def get_parser():
+    """
+    Get parser object for `eco_mocks_create.py` script.
+
+    Returns
+    -------
+    args: 
+        input arguments to the script
+    """
+    ## Define parser object
+    description_msg = 'Script to evaluate 1-halo conformity quenched fractions \
+                        on SDSS DR7'
+    parser = ArgumentParser(description=description_msg,
+                            formatter_class=SortingHelpFormatter,)
+    # 
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+    ## SDSS Sample
+    parser.add_argument('-sample',
+                        dest='sample',
+                        help='SDSS Luminosity sample to analyze',
+                        type=int,
+                        choices=[19,20,21],
+                        default=19)
+    ## SDSS Kind
+    parser.add_argument('-kind',
+                        dest='catl_kind',
+                        help='Type of data being analyzed.',
+                        type=str,
+                        choices=['data','mocks'],
+                        default='data')
+    ## SDSS Type
+    parser.add_argument('-abopt',
+                        dest='catl_type',
+                        help='Type of Abund. Matching used in catalogue',
+                        type=str,
+                        choices=['mr'],
+                        default='mr')
+    ## Total number of Iterations
+    parser.add_argument('-itern',
+                        dest='itern_tot',
+                        help='Total number of iterations to perform on the `shuffled` scenario',
+                        type=int,
+                        choices=range(10,10000),
+                        metavar='[10-10000]',
+                        default=1000)
+    ## Minimum Number of Galaxies in 1 group
+    parser.add_argument('-nmin',
+                        dest='ngals_min',
+                        help='Minimum number of galaxies in a galaxy group',
+                        type=int,
+                        default=2)
+    ## Bin in Group mass
+    parser.add_argument('-mg',
+                        dest='Mg_bin',
+                        help='Bin width for the group masses',
+                        type=_check_pos_val,
+                        default=0.4)
+    ## Logarithm of the galaxy property
+    parser.add_argument('-log',
+                        dest='prop_log',
+                        help='Use `log` or `non-log` units for `M*` and `sSFR`',
+                        type=str,
+                        choices=['log', 'nonlog'],
+                        default='log')
+    ## Mock Start
+    parser.add_argument('-catl_start',
+                        dest='catl_start',
+                        help='Starting index of mock catalogues to use',
+                        type=int,
+                        choices=range(101),
+                        metavar='[0-100]',
+                        default=0)
+    ## Mock Finish
+    parser.add_argument('-catl_finish',
+                        dest='catl_finish',
+                        help='Finishing index of mock catalogues to use',
+                        type=int,
+                        choices=range(101),
+                        metavar='[0-100]',
+                        default=100)
+    ## `Perfect Catalogue` Option
+    parser.add_argument('-perf',
+                        dest='perf_opt',
+                        help='Option for using a `Perfect` catalogue',
+                        type=_str2bool,
+                        default=False)
+    ## Type of correlation funciton to perform
+    parser.add_argument('-corrtype',
+                        dest='corr_type',
+                        help='Type of correlation function to perform',
+                        type=str,
+                        choices=['galgal'],
+                        default='galgal')
+    ## Shuffling Marks
+    parser.add_argument('-shuffle',
+                        dest='shuffle_marks',
+                        help='Option for shuffling marks of Cens. and Sats.',
+                        choices=['cen_sh', 'sat_sh', 'censat_sh'],
+                        default='censat_sh')
+    ## Option for removing file
+    parser.add_argument('-remove',
+                        dest='remove_files',
+                        help='Delete pickle file containing pair counts',
+                        type=_str2bool,
+                        default=False)
+    ## CPU Counts
+    parser.add_argument('-cpu',
+                        dest='cpu_frac',
+                        help='Fraction of total number of CPUs to use',
+                        type=float,
+                        default=0.75)
+    ## Show Progbar
+    parser.add_argument('-prog',
+                        dest='prog_bar',
+                        help='Option to print out progress bars for each for loop',
+                        type=_str2bool,
+                        default=True)
+    ## Program message
+    parser.add_argument('-progmsg',
+                        dest='Prog_msg',
+                        help='Program message to use throught the script',
+                        type=str,
+                        default=cu.Program_Msg(__file__))
+    ## Type of error estimation
+    parser.add_argument('-sigma',
+                        dest='type_sigma',
+                        help='Type of error to use. Percentiles or St. Dev.',
+                        type=str,
+                        choices=['std','perc'],
+                        default='std')
+    ## Parsing Objects
+    args = parser.parse_args()
+
+    return args
+
+def add_to_dict(param_dict):
+    """
+    Aggregates extra variables to dictionary
+
+    Parameters
+    ----------
+    param_dict: python dictionary
+        dictionary with input parameters and values
+
+    Returns
+    ----------
+    param_dict: python dictionary
+        dictionary with old and new values added
+    """
+    ### Sample - Int
+    sample_s = str(param_dict['sample'])
+    ### Perfect Catalogue
+    if param_dict['perf_opt']:
+        perf_str = 'haloperf'
+    else:
+        perf_str = ''
+    ### Figure
+    fig_idx = 24
+    ### Survey Details
+    sample_title = r'\boldmath$M_{r}< -%d$' %(param_dict['sample'])
+    ## Project Details
+    # String for Main directories
+    param_str_arr = [   param_dict['catl_kind'] , param_dict['catl_type']    ,
+                        param_dict['sample']    , param_dict['prop_log']     ,
+                        param_dict['Mg_bin']    , param_dict['itern_tot']    ,
+                        param_dict['ngals_min' ], param_dict['shuffle_marks'],
+                        param_dict['type_sigma'], perf_str  ]
+    param_str_p  = 'kind_{0}_type_{1}_sample_{2}_proplog_{3}_Mgbin_{4}_'
+    param_str_p += 'itern_{5}_nmin_{6}_sh_marks_{7}_type_sigma_{8}_'
+    param_str_p += 'perf_str_{9}'
+    param_str    = param_str_p.format(*param_str_arr)
+    ###
+    ### To dictionary
+    param_dict['sample_s'     ] = sample_s
+    param_dict['perf_str'     ] = perf_str
+    param_dict['fig_idx'      ] = fig_idx
+    param_dict['sample_title' ] = sample_title
+    param_dict['param_str'    ] = param_str
+    
+    return param_dict
+
+def param_vals_test(param_dict):
+    """
+    Checks if values are consistent with each other.
+
+    Parameters
+    -----------
+    param_dict: python dictionary
+        dictionary with `project` variables
+
+    Raises
+    -----------
+    ValueError: Error
+        This function raises a `ValueError` error if one or more of the 
+        required criteria are not met
+    """
+    ##
+    ## Check the `perf_opt` for when `catl_kind` is 'data'
+    if (param_dict['catl_kind']=='data') and (param_dict['perf_opt']):
+        msg = '{0} `catl_kind` ({1}) must smaller than `perf_opt` ({2})'\
+            .format(
+            param_dict['Prog_msg'],
+            param_dict['catl_kind'],
+            param_dict['perf_opt'])
+        raise ValueError(msg)
+    else:
+        pass
+    ##
+    ## Checking that `nmin` is larger than 2
+    if param_dict['ngals_min'] >= 2:
+        pass
+    else:
+        msg = '{0} `ngals_min` ({1}) must be larger than `2`'.format(
+            param_dict['Prog_msg'],
+            param_dict['ngals_min'])
+        raise ValueError(msg)
+    ##
+    ## Checking `cpu_frac` range
+    if (param_dict['cpu_frac'] > 0) and (param_dict['cpu_frac'] <= 1):
+        pass
+    else:
+        msg = '{0} `cpu_frac` ({1}) must be between (0,1]'.format(
+            param_dict['Prog_msg'],
+            param_dict['cpu_frac'])
+        raise ValueError(msg)
+    ##
+    ## Checking that `catl_start` < `catl_finish`
+    if param_dict['catl_start'] < param_dict['catl_finish']:
+        pass
+    else:
+        msg = '{0} `catl_start` ({1}) must smaller than `catl_finish` ({2})'\
+            .format(
+            param_dict['Prog_msg'],
+            param_dict['catl_start'],
+            param_dict['catl_finish'])
+        raise ValueError(msg)
+
+def directory_skeleton(param_dict, proj_dict):
+    """
+    Creates the directory skeleton for the current project
+
+    Parameters
+    ----------
+    param_dict: python dictionary
+        dictionary with `project` variables
+
+    proj_dict: python dictionary
+        dictionary with info of the project that uses the
+        `Data Science` Cookiecutter template.
+
+    Returns
+    ---------
+    proj_dict: python dictionary
+        Dictionary with current and new paths to project directories
+    """
+    ### MCF Folder prefix
+    path_prefix = 'SDSS/{0}/{1}/Mr{2}/Frac_results'.format(
+                        param_dict['catl_kind'],
+                        param_dict['catl_type'],
+                        param_dict['sample'   ])
+    ### MCF Output directory - Results
+    pickdir = '{0}/processed/{1}/{2}/catl_pickle_files/{3}/'.format(
+                    proj_dict['data_dir'],
+                    path_prefix          ,
+                    param_dict['corr_type'],
+                    param_dict['param_str'])
+    # Creating Folders
+    cu.Path_Folder(pickdir)
+    ## Adding to `proj_dict`
+    proj_dict['pickdir'   ] = pickdir
+
+    return proj_dict
+
+def sigma_calcs(data_arr, type_sigma='std', perc_arr = [68., 95., 99.7],
+    return_mean_std=False):
+    """
+    Calcualates the 1-, 2-, and 3-sigma ranges for `data_arr`
+
+    Parameters
+    -----------
+    data_arr: numpy.ndarray, shape( param_dict['nrpbins'], param_dict['itern_tot'])
+        array of values, from which to calculate percentiles or St. Dev.
+
+    type_sigma: string, optional (default = 'std')
+        option for calculating either `percentiles` or `standard deviations`
+        Options:
+            - 'perc': calculates percentiles
+            - 'std' : uses standard deviations as 1-, 2-, and 3-sigmas
+
+    perc_arr: array_like, optional (default = [68., 95., 99.7])
+        array of percentiles to calculate
+
+    return_mean_std: boolean, optional (default = False)
+        option for returning mean and St. Dev. along with `sigma_dict`
+
+    Return
+    ----------
+    sigma_dict: python dicitionary
+        dictionary containg the 1-, 2-, and 3-sigma upper and lower 
+        ranges for `data-arr`
+
+    mark_mean: array_like
+        array of the mean value of `data_arr`.
+        Only returned if `return_mean_std == True`
+
+    mark_std: array_like
+        array of the St. Dev. value of `data_arr`.
+        Only returned if `return_mean_std == True`
+    """
+    ## Creating dictionary for saving `sigma`s
+    sigma_dict = {}
+    for ii in range(len(perc_arr)):
+        sigma_dict[ii] = []
+    ## Using Percentiles to estimate errors
+    if type_sigma=='perc':
+        for ii, perc_ii in enumerate(perc_arr):
+            mark_lower = num.nanpercentile(data_arr, 50.-(perc_ii/2.),axis=1)
+            mark_upper = num.nanpercentile(data_arr, 50.+(perc_ii/2.),axis=1)
+            # Saving to dictionary
+            sigma_dict[ii] = num.column_stack((mark_lower, mark_upper)).T
+    ## Using standard deviations to estimate errors
+    if type_sigma=='std':
+        mean_val = num.nanmean(data_arr, axis=1)
+        std_val  = num.nanstd( data_arr, axis=1)
+        for ii in range(len(perc_arr)):
+            mark_lower = mean_val - ((ii+1) * std_val)
+            mark_upper = mean_val + ((ii+1) * std_val)
+            # Saving to dictionary
+            sigma_dict[ii] = num.column_stack((mark_lower, mark_upper)).T
+    ##
+    ## Estimating mean and St. Dev. of `data_arr`
+    mark_mean = num.nanmean(data_arr, axis=1)
+    mark_std  = num.nanstd (data_arr, axis=1)
+
+    if return_mean_std:
+        return sigma_dict, mark_mean, mark_std
+    else:
+        return sigma_dict
+
+## --------- DATA EXTRACTION ------------##
+
+## --------- Main Function ------------##
+
+def main():
+    """
+
+    """
+    ## Reading all elements and converting to python dictionary
+    param_dict = vars(args)
+    ## Checking for correct input
+    param_vals_test(param_dict)
+    ## ---- Adding to `param_dict` ---- 
+    param_dict = add_to_dict(param_dict)
+    ## Program message
+    Prog_msg = param_dict['Prog_msg']
+    ##
+    ## Creating Folder Structure
+    # proj_dict  = directory_skeleton(param_dict, cu.cookiecutter_paths(__file__))
+    proj_dict  = directory_skeleton(param_dict, cu.cookiecutter_paths('./'))
+    ##
+    ## Printing out project variables
+    print('\n'+50*'='+'\n')
+    for key, key_val in sorted(param_dict.items()):
+        if key !='Prog_msg':
+            print('{0} `{1}`: {2}'.format(Prog_msg, key, key_val))
+    print('\n'+50*'='+'\n')
+
+
+# Main function
+if __name__=='__main__':
+    ## Input arguments
+    args = get_parser()
+    # Main Function
+    main()
