43af2b4a943c927c4c856291ac0ee5af7a9b986b
Victor Calderon <victor.calderon90@gmail.com>
2017-10-27 04:38:28 -0500

    Added functions to estimate the quenched fractions of satellites

--- src/data/One_halo_conformity/one_halo_conformity_quenched_fractions.py
+++ src/data/One_halo_conformity/one_halo_conformity_quenched_fractions.py
@@ -29,6 +29,7 @@ from progressbar import (Bar, ETA, FileTransferSpeed, Percentage, ProgressBar,
 from argparse import ArgumentParser
 from argparse import HelpFormatter
 from operator import attrgetter
+import copy
 
 ## Functions
 
@@ -209,6 +210,13 @@ def get_parser():
                         type=str,
                         choices=['std','perc'],
                         default='std')
+    ## Statistics for evaluating conformity
+    parser.add_argument('-frac_stat',
+                        dest='frac_stat',
+                        help='Statistics to use to evaluate the conformity signal',
+                        type=str,
+                        choices=['diff', 'ratio'],
+                        default='diff')
     ## Parsing Objects
     args = parser.parse_args()
 
@@ -245,10 +253,11 @@ def add_to_dict(param_dict):
                         param_dict['sample']    , param_dict['prop_log']     ,
                         param_dict['Mg_bin']    , param_dict['itern_tot']    ,
                         param_dict['ngals_min' ], param_dict['shuffle_marks'],
-                        param_dict['type_sigma'], perf_str  ]
+                        param_dict['type_sigma'], param_dict['frac_stat']    ,
+                        perf_str  ]
     param_str_p  = 'kind_{0}_type_{1}_sample_{2}_proplog_{3}_Mgbin_{4}_'
     param_str_p += 'itern_{5}_nmin_{6}_sh_marks_{7}_type_sigma_{8}_'
-    param_str_p += 'perf_str_{9}'
+    param_str_p += 'fracstat_{9}_perf_str_{10}'
     param_str    = param_str_p.format(*param_str_arr)
     ###
     ### To dictionary
@@ -420,9 +429,324 @@ def sigma_calcs(data_arr, type_sigma='std', perc_arr = [68., 95., 99.7],
 
 ## --------- Analysis functions ------------##
 
+def frac_prop_calc(df_bin_org, prop, GM_str, param_dict, catl_keys_dict):
+    """
+    Computes the quenched fractions of satellites in a given mass bin.
+
+    Parameters
+    ----------
+    df_bin_org: pandas DataFrame
+        Dataframe for the selected group/halo mass bin
+
+    prop: string
+        galaxy property being evaluated
+
+    GM_str: string
+        string for the corresponding group/halo mass bin limits
+
+    param_dict: python dictionary
+        dictionary with input parameters and values
+
+    catl_keys_dict: python dictionary
+        dictionary containing keys for the galaxy properties in catalogue
+
+    Returns
+    ----------
+    """
+    ## Program message
+    Prog_msg = param_dict['Prog_msg']
+    ## Constants
+    Cens     = int(1)
+    Sats     = int(0)
+    itern    = param_dict['itern_tot']
+    ## Catalogue Variables for galaxy properties
+    gm_key      = catl_keys_dict['gm_key']
+    id_key      = catl_keys_dict['id_key']
+    galtype_key = catl_keys_dict['galtype_key']
+    ## Group statistics
+    groupid_unq = df_bin_org[id_key].unique()
+    ngroups     = groupid_unq.shape[0]
+    ##
+    ## Selecting columns
+    df_bin_mod = df_bin_org.copy()[[galtype_key, id_key, prop]]
+    ##
+    ## Normalizing `prop` by the `prop_lim`
+    df_bin_mod.loc[:,prop] /= param_dict['prop_lim'][prop]
+    ##
+    ## Determining galaxy fractions for `df_bin_mod`
+    cens_pd_org = df_bin_mod.loc[(df_bin_mod[galtype_key]==Cens)].copy().reset_index()
+    sats_pd_org = df_bin_mod.loc[(df_bin_mod[galtype_key]==Sats)].copy().reset_index()
+    ##
+    ## Quench Satellite fraction
+    sat_quenched_frac = frac_stat_calc( cens_pd_org   ,
+                                        sats_pd_org   ,
+                                        prop          ,
+                                        catl_keys_dict,
+                                        param_dict    ,
+                                        frac_stat=param_dict['frac_stat'])
+    ##
+    ## Converting `sat_quenched_frac` to numpy array
+    sat_quenched_frac = sat_quenched_frac
+    ##
+    ## Calculation fractions for Shuffles
+    sat_quenched_frac_sh = num.zeros((param_dict['itern_tot'], 1))
+    # ProgressBar properties
+    if param_dict['prog_bar']:
+        widgets   = [Bar('>'), ' ', ETA(), ' ', ReverseBar('<')]
+        pbar_mock = ProgressBar( widgets=widgets, maxval= 10 * itern).start()
+    ## Iterating `itern` times and calculating quenched fractions
+    for ii in range(itern):
+        ## Quenched fractions
+        sat_quenched_frac_sh[ii] = frac_stat_calc(cens_pd_org ,
+                                                sats_pd_org   ,
+                                                prop          ,
+                                                catl_keys_dict,
+                                                param_dict    ,
+                                                shuffle=True  ,
+                                                frac_stat=param_dict['frac_stat'])
+        if param_dict['prog_bar']:
+            pbar_mock.update(ii)
+    if param_dict['prog_bar']:
+        pbar_mock.finish()
+
+    return sat_quenched_frac, sat_quenched_frac_sh.T
+
+def frac_stat_calc(cens_pd_org, sats_pd_org, prop, catl_keys_dict, param_dict, 
+    frac_stat='diff', shuffle=False):
+    """
+    Computes quenched fractions of satellites for a given galaxy property 
+    `prop` in a given mass bin.
+
+    Parameters
+    ----------
+    cens_pd_org: pandas DataFrame
+        dataframe with only central galaxies in the given group mass bin.
+        Centrals belong to groups with galaxies >= `param_dict['ngals_min']`
+
+    sats_pd_org: pandas DataFrame
+        dataframe with only satellite galaxies in the given group mass bin.
+        Satellites belong to groups with galaxies >= `param_dict['ngals_min']`
+
+    prop: string
+        galaxy property being analyzed
+
+    catl_keys_dict: python dictionary
+        dictionary containing keys for the galaxy properties in catalogue
+
+    param_dict: python dictionary
+        dictionary with input parameters and values
+
+    frac_stat: string, optional (default = 'diff')
+        statistics to use to evaluate the conformity signal
+        Options:
+            - 'diff' : Takes the difference between P(sat=q|cen=q) and 
+                        P(sat=q|cen=act)
+            - 'ratio': Takes the ratio between P(sat=q|cen=q) and 
+                        P(sat=q|cen=act)
+
+    shuffle: boolean, optional (default = False)
+        option for shuffling the galaxies' properties among themselves, i.e. 
+        centrals among centrals, and satellites among satellites.
+
+    Returns
+    -------
+    frac_sat_pas_cen_act: float
+        number of `passive` satellites around `active` centrals over the 
+        total number of satelltes around `active` centrals
+
+    frac_sat_pas_cen_pas: float
+        number of `passive` satellites around `passive` centrals over the 
+        total number of satelltes around `passive` centrals
+
+    frac_stat: float
+        'ratio' or 'difference' of between P(sat=q|cen=q) and P(sat=q|cen=act)
+
+    """
+    ## Keys for group/halo ID, mass, and galaxy type
+    gm_key      = catl_keys_dict['gm_key']
+    id_key      = catl_keys_dict['id_key']
+    galtype_key = catl_keys_dict['galtype_key']
+    ## Copies of `cens_pd_org` and `sats_pd_org`
+    cens_pd = cens_pd_org.copy()
+    sats_pd = sats_pd_org.copy()
+    ## Choosing if to shuffle `prop`
+    if shuffle:
+        ## Choosing which kind of shuffle to use
+        # Shuffling only Centrals
+        if param_dict['shuffle_marks'] == 'cen_sh':
+            cens_prop_sh = cens_pd[prop].copy().values
+            num.random.shuffle(cens_prop_sh)
+            cens_pd.loc[:,prop] = cens_prop_sh
+        # Shuffling only Satellites
+        if param_dict['shuffle_marks'] == 'sat_sh':
+            sats_prop_sh = sats_pd[prop].copy().values
+            num.random.shuffle(sats_prop_sh)
+            sats_pd.loc[:,prop] = sats_prop_sh
+        # Shuffling both Centrals and Satellites
+        if param_dict['shuffle_marks'] == 'censat_sh':
+            # Centrals
+            cens_prop_sh = cens_pd[prop].copy().values
+            num.random.shuffle(cens_prop_sh)
+            cens_pd.loc[:,prop] = cens_prop_sh
+            # Satellites
+            sats_prop_sh = sats_pd[prop].copy().values
+            num.random.shuffle(sats_prop_sh)
+            sats_pd.loc[:,prop] = sats_prop_sh
+    ##
+    ## Separating fractions for Centrals and Satellites
+    cens_act = cens_pd.loc[(cens_pd[prop]) <= 1]
+    cens_pas = cens_pd.loc[(cens_pd[prop]) >  1]
+    ##
+    ## Groups for each `act` and `pas` centrals
+    cens_act_groups = cens_act[id_key].values
+    cens_pas_groups = cens_pas[id_key].values
+    ##
+    ## Satellites residing in groups/halos with `act` and `pas` centrals
+    sats_c_act     = sats_pd.loc[(sats_pd[id_key].isin(cens_act_groups))]
+    sats_c_pas     = sats_pd.loc[(sats_pd[id_key].isin(cens_pas_groups))]
+    ## Total number of satellites in around each type of central
+    sats_c_act_tot = len(sats_c_act)
+    sats_c_pas_tot = len(sats_c_pas)
+    ##
+    ## Number of quenched satellites around each type of centrals
+    sats_pas_c_act     = sats_c_act.loc[sats_c_act[prop] > 1]
+    sats_pas_c_pas     = sats_c_pas.loc[sats_c_pas[prop] > 1]
+    sats_pas_c_act_tot = len(sats_pas_c_act)
+    sats_pas_c_pas_tot = len(sats_pas_c_pas)
+    ##
+    ## Quenched fractions of satellites
+    # Passive Satellites around `active` centrals
+    if sats_c_act_tot != 0:
+        frac_sat_pas_cen_act = sats_pas_c_act_tot/float(sats_c_act_tot)
+    else:
+        frac_sat_pas_cen_act = num.nan
+    # Passive Satellites around `passive` centrals
+    if sats_c_pas_tot != 0:
+        frac_sat_pas_cen_pas = sats_pas_c_pas_tot/float(sats_c_pas_tot)
+    else:
+        frac_sat_pas_cen_pas = num.nan
+    ##
+    ## Evaluating `frac_stat`
+    # Taking the difference of fractions
+    if frac_stat=='diff':
+        frac_res = (frac_sat_pas_cen_pas - frac_sat_pas_cen_act)
+    # Taking the ratio of fractions
+    if frac_stat == 'ratio':
+        frac_res = (frac_sat_pas_cen_pas / frac_sat_pas_cen_act)
+
+    return frac_res
+
 def gm_fractions_calc(catl_pd, catl_name, param_dict, proj_dict):
     """
+    Computes the 'quenched' satellite fractions for galaxy groups.
+    Splits the sample into group mass bins, and determines the quenched 
+    fraction of satellite for a given galaxy property.
+
+    Parameters
+    ----------
+    catl_pd: pandas DataFrame
+        DataFrame with information on catalogue
+
+    catl_name: string
+        name of the `catl_pd`
+
+    param_dict: python dictionary
+        dictionary with `project` variables
+
+    proj_dict: python dictionary
+        Dictionary with current and new paths to project directories
+
     """
+    Prog_msg = param_dict['Prog_msg']
+    ### Catalogue Variables
+    # `Group mass`, `groupid`, and `galtype` keys
+    gm_key, id_key, galtype_key = cu.catl_keys(catl_kind=param_dict['catl_kind'],
+                                                return_type='list',
+                                                perf_opt=param_dict['perf_opt'])
+    catl_keys_dict = cu.catl_keys(  catl_kind=param_dict['catl_kind'],
+                                    return_type='dict',
+                                    perf_opt=param_dict['perf_opt'])
+    gm_key      = catl_keys_dict['gm_key']
+    id_key      = catl_keys_dict['id_key']
+    galtype_key = catl_keys_dict['galtype_key']
+    # ssfr and mstar keys
+    ssfr_key, mstar_key = cu.catl_keys_prop(catl_kind=param_dict['catl_kind'], 
+                                                catl_info='members')
+    # Galaxy Properties
+    if param_dict['catl_kind']=='data':
+        pd_keys     = ['logssfr', 'g_r', 'sersic']
+    elif param_dict['catl_kind']=='mocks':
+        pd_keys = ['logssfr']
+    # Limits for each galaxy property
+    prop_lim = {'logssfr':-11,
+                'sersic':3,
+                'g_r':0.75}
+    param_dict['prop_lim'] = prop_lim
+    # Cleaning catalogue with groups of N > `ngals_min`
+    catl_pd_clean = cu.sdss_catl_clean_nmin(catl_pd, param_dict['catl_kind'],
+        nmin=param_dict['ngals_min'])
+    ### Mass limits
+    GM_min  = catl_pd_clean[gm_key].min()
+    GM_max  = catl_pd_clean[gm_key].max()
+    GM_arr  = cu.Bins_array_create([GM_min,GM_max], param_dict['Mg_bin'])
+    GM_bins = [[GM_arr[ii],GM_arr[ii+1]] for ii in range(GM_arr.shape[0]-1)]
+    GM_bins = num.asarray(GM_bins)
+    GM_keys = ['{0:.2f}_{1:.2f}'.format(GM_arr[xx],GM_arr[xx+1])\
+                    for xx in range(len(GM_arr)-1)]
+    ### Pickle file
+    p_file = [  proj_dict['pickdir']       , param_dict['fig_idx']  ,
+                catl_name                  , param_dict['sample']   ,
+                param_dict['corr_type']    , param_dict['prop_log'] ,
+                param_dict['shuffle_marks'], param_dict['ngals_min'],
+                param_dict['perf_str']     ]
+    p_fname = '{0}/{1}_{2}_Mr{3}_{4}_{5}_{6}_{7}_{8}.p'
+    p_fname = p_fname.format(*p_file)
+    ##
+    ## Checking if file exists
+    if (os.path.isfile(p_fname)) and (param_dict['remove_files']):
+        os.remove(p_fname)
+        print('{0} `p_fname` ({1}) removed! Calculating MCF statistics!'.format(
+            Prog_msg, p_fname))
+    ## Dictionary for storing results for each GM bin and galaxy property
+    frac_gm_dict = dict(zip(GM_keys, [[] for x in range(len(GM_keys))]))
+    stat_vals    = [num.zeros((len(GM_keys))), copy.deepcopy(frac_gm_dict)]
+    GM_prop_dict = dict(zip(pd_keys,[list(stat_vals) for x in range(len(pd_keys))]))
+    ## Looping ovr each GM bin (if file does not exist for the given catalogue)
+    if not (os.path.isfile(p_fname)):
+        ## Looping over mass bins
+        for ii, GM_ii in enumerate(GM_bins):
+            # GM Key label
+            GM_key = GM_keys[ii]
+            # GM string
+            GMbin_min, GMbin_max = GM_ii
+            if param_dict['perf_opt']:
+                print('\n{0} Halo Mass range: {1}'.format(Prog_msg, GM_keys))
+            else:
+                print('\n{0} Group Mass range: {1}'.format(Prog_msg, GM_key))
+            df_bin_org = catl_pd_clean.loc[ (catl_pd_clean[gm_key] >= GMbin_min) &\
+                                        (catl_pd_clean[gm_key] <  GMbin_max)].copy()
+            df_bin_org.reset_index(inplace=True, drop=True)
+            ##
+            ## Looping over galaxy properties
+            for jj, prop in enumerate(pd_keys):
+                print('{0} >> Galaxy Prop: {1}'.format(Prog_msg, prop))
+                (   sat_quenched_frac,
+                    sat_quenched_frac_sh) = frac_prop_calc( df_bin_org    ,
+                                                            prop          ,
+                                                            GM_str        ,
+                                                            param_dict    ,
+                                                            catl_keys_dict)
+                ##
+                ## Saving results to dictionary
+                prop_dict[prop][0][ii]     = sat_quenched_frac
+                prop_dict[prop][1][GM_key] = sat_quenched_frac_sh
+    ##
+    ## Saving `prop_dict` to Pickle file
+    print('{0} Saving data to Pickle: \n\t{1}\n'.format(Prog_msg, p_fname))
+    p_data = [param_dict, prop_dict, GM_arr, GM_bins, GM_keys]
+    pickle.dump(p_data, open(p_fname,'wb'))
+    print('{0} Data saved to Pickle: \n\t{1}\n'.format(Prog_msg, p_fname))
+
 ## --------- Multiprocessing ------------##
 
 def multiprocessing_catls(catl_arr, param_dict, proj_dict, memb_tuples_ii):
