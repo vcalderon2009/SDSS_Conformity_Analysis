b0e8a97fbcbf0b84b2d5b2fc79f1e762399bde08
Victor Calderon <victor.calderon90@gmail.com>
2018-02-09 09:26:32 -0600

    Removed 'fancybox'

--- src/data/referee_report_src/Referee_Report_analysis.py
+++ src/data/referee_report_src/Referee_Report_analysis.py
@@ -846,7 +846,7 @@ def projected_wp_plot(act_pd_data, pas_pd_data, act_pd_mock, pas_pd_mock,
     ## Legend
     ax_data.legend( loc='lower left', bbox_to_anchor=[0, 0],
                     ncol=2, shadow=True, title='Galaxy Properties',
-                    fancybox=True, prop={'size':size_legend})
+                    prop={'size':size_legend})
     ax_data.get_legend().get_title().set_color("red")
     ax_data.set_xscale('log')
     ax_data.set_yscale('log')
